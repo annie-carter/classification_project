fcacfc5042149610e06d9e1ab81f10605e3f7b81
annie-carter <annie.carter831@gmail.com>
2023-06-20 14:10:03 -0500

    acquire.py updated

--- acquire.py
+++ acquire.py
@@ -2,54 +2,25 @@
  "cells": [
   {
    "cell_type": "code",
-   "execution_count": 4,
-   "id": "5fa08b70",
+   "execution_count": 8,
+   "id": "a049c55b",
    "metadata": {},
    "outputs": [],
    "source": [
     "import pandas as pd\n",
     "import os as os\n",
     "import csv\n",
-    "from env import get_db_url\n"
+    "from env import user, password, hostname, get_db_url\n"
    ]
   },
   {
    "cell_type": "code",
-   "execution_count": 10,
-   "id": "b745b8e1",
+   "execution_count": 6,
+   "id": "fab5da0b",
    "metadata": {},
    "outputs": [],
    "source": [
-    "def get_telco_data():\n",
-    "#     '''\n",
-    "#     # Retrieves the telco dataset either from the codeup server or a csv if its already been imported, returns a dataframe\n",
-    "#     '''\n",
-    "    url = get_db_url('telco_churn')\n",
-    "    query = '''SELECT *\n",
-    "             FROM customers\n",
-    "             JOIN payment_types USING (payment_type_id)\n",
-    "             JOIN contract_types USING (contract_type_id)\n",
-    "             JOIN internet_service_types USING (internet_service_type_id);'''\n",
-    "    filename = 'telco.csv'\n",
-    "    if os.path.isfile(filename):\n",
-    "        return pd.read_csv(filename)\n",
-    "    else:\n",
-    "        df = pd.read_sql(query, url)\n",
-    "        df.to_csv(filename)\n",
-    "        return df\n",
-    "\n",
-    "    \n",
-    "    "
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 9,
-   "id": "3a2f509f",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def get_telco_data():\n",
+    "def get_telco_data(df):\n",
     "    filename = \"telco.csv\"\n",
     "    if os.path.isfile(filename):\n",
     "        return pd.read_csv(filename)\n",
@@ -60,8 +31,8 @@
     "                            JOIN contract_types USING (contract_type_id)\n",
     "                            JOIN internet_service_types USING (internet_service_type_id)\n",
     "                            JOIN customer_subscriptions USING (customer_id)\n",
-    "                            JOIN customer_contracts USING(customer_id)\n",
     "                            JOIN customer_payments USING (customer_id)\n",
+    "                            JOIN customer_contracts USING(customer_id)\n",
     "                            JOIN customer_signups USING (customer_id)''',\n",
     "                         url=get_db_url('telco_churn'))\n",
     "\n",
@@ -70,6 +41,28 @@
     "\n",
     "        # Return the dataframe to the calling code\n",
     "        return df\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "d4f7e423",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def get_telco_data_with_date(df):\n",
+    "    filename = \"telcowdate.csv\"\n",
+    "    if os.path.isfile(filename):\n",
+    "        return pd.read_csv(filename)\n",
+    "    else:\n",
+    "        # read the SQL  datafra\n",
+    "        df = pd.read_sql('SELECT * FROM customers JOIN customer_signups USING (customer_id)', get_connection('telco_churn'))\n",
+    "\n",
+    "        # Write that dataframe to disk for later. Called \"caching\" the data for later.\n",
+    "        df.to_csv(filename,index=False)\n",
+    "\n",
+    "        # Return the dataframe to the calling code\n",
+    "        return df  "
    ]
   }
  ],
