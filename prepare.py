34871cdf74d03373e8d35e0fa01057dd978fa7c9
annie-carter <annie.carter831@gmail.com>
2023-06-26 01:48:55 -0500

    final prepare.py

--- prepare.py
+++ prepare.py
@@ -14,8 +14,8 @@ from sklearn.metrics import confusion_matrix
 
 # prepare telco data
 def prep_telco(df):
-    df_telco=acquire.get_telco_data()
-    df_telco.drop(columns=['internet_service_type_id','contract_type_id','payment_type_id'],inplace=True)
+    df_telco=acquire.get_telco_data(df)
+    df_telco.drop(columns=['internet_service_type_id','contract_type_id','payment_type_id', 'Unnamed: 0'],inplace=True)
     dummy_df_telco = pd.get_dummies(df_telco[['partner','dependents','phone_service','multiple_lines','online_security','online_backup','device_protection','tech_support','streaming_tv','streaming_movies','paperless_billing','churn','internet_service_type','gender']], dummy_na=False, drop_first=[True])
     df_telco=pd.concat([df_telco ,dummy_df_telco],axis=1)
     df_telco.drop(columns=['partner','dependents','phone_service','multiple_lines','online_security','online_backup','device_protection','tech_support','streaming_tv','streaming_movies','paperless_billing','churn','internet_service_type','gender'],inplace=True)
@@ -26,6 +26,6 @@ def prep_telco(df):
 def split_telco(df):
     df_telco = prep_telco(df)
     train, telco_test = train_test_split(df_telco, test_size=0.2, random_state=123)
-    telco_train, telco_validate = train_test_split(train, test_size=0.25, random_state=123)
-    
+    telco_train, telco_validate = train_test_split(train, test_size=0.25, random_state=123)   
     return telco_train, telco_validate, telco_test
+
